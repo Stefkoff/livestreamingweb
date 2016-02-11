14666ae2bc2bf627a690d9b7ddb018b0f7b0bf4a
stefkoff <stefkoff@gmail.com>
2016-02-12 01:15:38 +0200

    Added .gitignore and test for crontab

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Live', 'url' => ['/site/live']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ?
