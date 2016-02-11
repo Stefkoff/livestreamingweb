48da414e849ab37f02dc1f4d91cf681b004cdb48
stefkoff <stefkoff@gmail.com>
2016-02-12 01:54:00 +0200

    test 2

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Live 2', 'url' => ['/site/live']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ?
