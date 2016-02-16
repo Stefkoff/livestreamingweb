5fa410ac12888d33f0e246ca6b9264156b15d634
Stefkoff <stefkoff@gmail.com>
2016-02-15 21:33:14 -0800

    Creating pritty URL

--- config/web.php
+++ config/web.php
@@ -38,6 +38,18 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
     'params' => $params,
 ];
