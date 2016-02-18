5228d9827b0268b3ce297012c4e23d6907c1abc7
stefkoff <stefkoff@gmail.com>
2016-02-18 19:29:03 +0200

    Adding some comments

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "\n\n==> Updating code from git..."
 git reset --hard && git pull
 
 echo "\n\n==> Runing migrations..."
-
 php yii migrate --interactive=0
 
+echo "\n\n==> Deleting assets..."
 rm -Rf web/assets/*
